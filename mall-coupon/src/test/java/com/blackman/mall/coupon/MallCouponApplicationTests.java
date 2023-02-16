e6008f383c3895afcb202a0d8bf702c090333e06
blackman <blackman@gmail.com>
2023-02-16 10:39:33 +0800

    项目初始化结构

---
+++ mall-coupon/src/test/java/com/blackman/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackman.mall.coupon;
+
+/*import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}*/
