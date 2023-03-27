c6a700ba3dd225c2c9c87a7bd5dd63a71f476a6e
blackman <blackman@gmail.com>
2023-03-27 12:35:23 +0800

    商城后台三级菜单逻辑删除

--- mall-product/src/main/java/com/blackman/mall/product/controller/CategoryController.java
+++ mall-product/src/main/java/com/blackman/mall/product/controller/CategoryController.java
@@ -83,10 +83,11 @@ public class CategoryController {
      * 删除
      */
     @RequestMapping("/delete")
-    @RequiresPermissions("product:category:delete")
+    //@RequiresPermissions("product:category:delete")
     public R delete(@RequestBody Long[] catIds){
 		categoryService.removeByIds(Arrays.asList(catIds));
 
+        categoryService.removeMenusByIds(catIds);
         return R.ok();
     }
 
--- mall-product/src/main/java/com/blackman/mall/product/service/impl/CategoryServiceImpl.java
+++ mall-product/src/main/java/com/blackman/mall/product/service/impl/CategoryServiceImpl.java
@@ -2,6 +2,7 @@ package com.blackman.mall.product.service.impl;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -33,22 +34,26 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
     @Override
     public List<CategoryEntity> queryCategoryTree() {
         // 1.从数据库中查找出所有的分类信息
+        // 查出所有(包括一级、二级、三级等等)的分类信息
         List<CategoryEntity> categoryEntities = baseMapper.selectList(null);
 
         // 2.1首先找出第一级分类 标志为parent_cid=0
         // 2.2 查找二级分类
-        List<CategoryEntity> level1Menu = categoryEntities.stream()
-                .filter(categoryEntity -> categoryEntity.getParentCid() == 0L)
+        List<CategoryEntity> leve1Menu = categoryEntities.stream().filter(categoryEntity -> categoryEntity.getParentCid() == 0L)
                 .map((menu) -> {
-                    //设置二级分类
                     menu.setChildren(getChildrenTree(menu, categoryEntities));
                     return menu;
                 }).sorted((menu1, menu2) -> {
                     return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
                 }).collect(Collectors.toList());
+        return leve1Menu;
+    }
 
-        return level1Menu;
+    @Override
+    public void removeMenusByIds(Long[] catIds) {
+        //TODO 需判断在那些情况下不能进行删除
 
+        baseMapper.deleteBatchIds(Arrays.asList(catIds));
     }
 
     /**
@@ -58,9 +63,8 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
      * @return 返回当前菜单下的所有子菜单集合
      */
     private List<CategoryEntity> getChildrenTree(CategoryEntity root,  List<CategoryEntity> all) {
-        List<CategoryEntity> children = all.stream()
-                .filter(categoryEntity -> categoryEntity.getParentCid() == root.getCatId())
-                .map((menu) -> {
+        List<CategoryEntity> children = all.stream().filter(category -> (category.getParentCid() == root.getCatId()))
+                .map(menu -> {
                     menu.setChildren(getChildrenTree(menu, all));
                     return menu;
                 }).sorted((menu1, menu2) -> {
